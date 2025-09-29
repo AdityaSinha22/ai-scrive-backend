6cb21e7ad5def69e4aeadea43d174ae669e02551
AdityaSinha22 <115683485+AdityaSinha22@users.noreply.github.com>
2025-09-29 05:32:36 +0530

    Update upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const { handleUpload } = require("../controllers/uploadController");
+const { handleUpload } = require("../controllers/uploadcontroller");
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
@@ -8,3 +8,4 @@ const upload = multer({ dest: "uploads/" });
 router.post("/", upload.single("file"), handleUpload);
 
 module.exports = router;
+
